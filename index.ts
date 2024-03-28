4d6b294e3884543b29a0f90315166d67f78cd285
Hamid ali <hamidalikhokhar02@gmail.com>
2024-03-28 00:32:13 -0700

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const asnwer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to  perform action",
     type: "list",
@@ -13,7 +13,6 @@ const asnwer = await inquirer.prompt([
   },
 ]);
 
-// conditional statement
 if (asnwer.operator === "Addition") {
     console.log(asnwer.firstNumber + asnwer.secondNumber);
 } else if (asnwer.operator === "Subtraction") {
@@ -26,3 +25,4 @@ else if (asnwer.operator === "Division"){
 } else {
     console.log("Please select valid operator")
 }
+console.log("THE END");
